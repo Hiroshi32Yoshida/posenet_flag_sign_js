6432a6f3e49869f768f23d3b8d3179cbd7c805ce
Hiroshi <64144258+Hiroshi32Yoshida@users.noreply.github.com>
2021-01-04 21:48:08 +0900

    Update camera.js

--- camera.js
+++ camera.js
@@ -121,7 +121,7 @@ function setupFPS() {
  * happens. This function loops with a requestAnimationFrame method.
  */
 function detectPoseInRealTime(video, net) {
-  const canvas = document.getElementById('canvas');
+  const canvas = document.getElementById('output');
   const ctx = canvas.getContext('2d');
 
   // since images are being fed from a webcam, we want to feed in the
